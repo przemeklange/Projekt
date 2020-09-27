0cf2ccd3a6576af6832f4fdc08d254117cea0449
Przemysław Lange <69763132+przemeklange@users.noreply.github.com>
2020-09-27 19:38:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$('.slider').slick({
+
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 880,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
